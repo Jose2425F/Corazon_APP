dd8067fc5afef725413803d51028efd884d67b23
jose <josegalvanpayares@gmail.com>
2023-09-30 12:56:27 -0500

    boton 4

--- app/src/main/java/com/example/corazonapp/Login.kt
+++ app/src/main/java/com/example/corazonapp/Login.kt
@@ -17,7 +17,7 @@ class Login : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         val btn2: Button = findViewById(R.id.crear_cuenta)
-        val btn3: Button = findViewById(R.id.crear_cuenta)
+        val btn4: Button = findViewById(R.id.crear_cuenta)
 
         btn2.setOnClickListener{
             val intent: Intent = Intent(this, Registro_User::class.java)
